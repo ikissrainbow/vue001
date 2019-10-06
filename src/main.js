1d19bdcb07800ee752bd0a4dc302f2ce1e2fd1d1
ikissrainbow <2023742047@qq.com>
2019-10-06 20:47:05 +0800

    第一次提交

--- src/main.js
+++ src/main.js
@@ -1,15 +1,27 @@
 // 入口文件
 import Vue from 'vue'
-import app from './App.vue'
+// 导入vue-router并安装
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+// 导入vue-resource并安装
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+
 
 // 导入mui
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
 
 // 按需导入mint-ui
-import { Header } from 'mint-ui'
+import { Header, Swipe, SwipeItem  } from 'mint-ui'
 Vue.component(Header.name, Header)
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
 
+import router from './router.js'
+import app from './App.vue'
 var vm = new Vue({
     el: '#app',
-    render:c => c(app)
+    render:c => c(app),
+    router
 })
